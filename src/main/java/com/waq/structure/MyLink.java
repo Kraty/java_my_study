3504fd0b155844eeb844598e50f38ecc8f522426
Kraty <1457659929@qq.com>
2021-02-18 10:39:42 +0800

    基础复习-实现链表的增删查改

--- src/main/java/com/waq/structure/MyLink.java
+++ src/main/java/com/waq/structure/MyLink.java
@@ -15,12 +15,14 @@ public class MyLink {
         myLink.add(3);
         myLink.add(4);
         myLink.add(5);
-        System.out.println(myLink.get(4));
+        myLink.delete(0);
+        myLink.print();
 
     }
 
     private Node head = null;
 
+    // 增加节点
     public void add(int data) {
 
         Node node = new Node();
@@ -30,13 +32,47 @@ public class MyLink {
 
     }
 
-    public Integer get(int index) {
+    // 删除节点
+    public void delete(int index) {
+
+        Node preNode = find(index - 1);
+        Node nextNode = find(index + 1);
+        Node node = find(index);
+
+        if (node.getNext() != null) {
+
+            if (index == 0) {
+                head = node.getNext();
+            } else {
+                preNode.setNext(nextNode);
+            }
+
+        } else {
+            preNode.setNext(null);
+        }
+
+    }
+
+    // 查找节点
+    public Node find(int index) {
 
         Node node = head;
         for (int i = 0; i < index; i++) {
             node = node.getNext();
         }
-        return node.getData();
+        return node;
+
+    }
+
+    // 显示全部
+    public void print() {
+
+        while (head != null) {
+
+            System.out.println(head.getData());
+            head = head.getNext();
+
+        }
 
     }
 
